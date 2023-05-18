e5223c05205b97f44970dbaeac735636ba21b461
Miguelle-Obispo <miguelleobispo@gmail.com>
2023-05-18 17:44:43 +0800

    index main

---
+++ index.js
@@ -0,0 +1,13 @@
+var luzon = document.getElementById("luzon");
+var visayas = document.getElementById("visayas");
+var mindanao = document.getElementById("mindanao");
+
+function disappear1() {
+  luzon.classList.toggle("visib");
+}
+function disappear2() {
+  visayas.classList.toggle("visib");
+}
+function disappear3() {
+  mindanao.classList.toggle("visib");
+}
