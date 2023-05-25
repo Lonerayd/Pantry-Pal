5d091875da8907908efb503314ea080b3d5bd4ba
Lonerayd <dumalaog@yahoo.com>
2023-05-25 12:46:32 +0800

    fix nav

--- nav.js
+++ nav.js
@@ -1,31 +1,49 @@
 
 window.addEventListener("scroll", function() {
-    var navbar = document.querySelector(".desktopnav");
+    var navbar = document.querySelector("nav");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
 
    
   });
         // sidebar
-        const navbarToggle = document.querySelector('.navbar-toggle');
-        const sidebar = document.querySelector('.sidebar');
-        const sidebarToggle = document.querySelector('.sidebar-toggle');
+        // const navbarToggle = document.querySelector('.navbar-toggle');
+        // const sidebar = document.querySelector('.sidebar');
+        // const sidebarToggle = document.querySelector('.sidebar-toggle');
         
       
-        navbarToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-        });
-        sidebarToggle.addEventListener('click', () => {
-        sidebar.classList.remove('active');
-        });
+        // navbarToggle.addEventListener('click', () => {
+        // sidebar.classList.toggle('active');
+        // });
+        // sidebarToggle.addEventListener('click', () => {
+        // sidebar.classList.remove('active');
+        // });
 
        
 
         
 
-        document.addEventListener('click', (event) => {
-        const targetElement = event.target;
-        if (!sidebar.contains(targetElement) && !navbarToggle.contains(targetElement)) {
-            sidebar.classList.remove('active');
-        }
-        });
+        // document.addEventListener('click', (event) => {
+        // const targetElement = event.target;
+        // if (!sidebar.contains(targetElement) && !navbarToggle.contains(targetElement)) {
+        //     sidebar.classList.remove('active');
+        // }
+        // });
         // sidebar
+
+        // story js
+        const navbarToggle = document.querySelector('.fa-bars');
+        const links = document.querySelector(".nav-links")
+
+        $(document).ready(function () {
+            $('.burger').click(function () {
+                $('ul').toggleClass('show');
+            });
+        });
+
+        document.addEventListener('click', (event) => {
+            const targetElement = event.target;
+            if (!navbarToggle.contains(targetElement)) {
+                $(links).removeClass('show');
+            }
+            });
+        // story js
