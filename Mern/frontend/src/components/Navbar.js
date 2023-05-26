297ac275517cc4c9008668ee187751bb9318d88e
Lonerayd <dumalaog@yahoo.com>
2023-05-26 10:24:30 +0800

    save point

--- Mern/frontend/src/components/Navbar.js
+++ Mern/frontend/src/components/Navbar.js
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
-
+// import NavCSS from './css/style.module.css'
+// import logo from './images/logo.png'
 const Navbar = () => {
   const { logout } = useLogout()
   const { user } = useAuthContext()
@@ -12,25 +13,43 @@ const Navbar = () => {
 
   return (
     <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Workout Buddy</h1>
-        </Link>
-        <nav>
+      
           {user && (
             <div>
               <span>{user.email}</span>
               <button onClick={handleClick}>Log out</button>
             </div>
+           
+
           )}
           {!user && (
             <div>
               <Link to="/login">Login</Link>
               <Link to="/signup">Signup</Link>
             </div>
+
+      //       <nav className="navbar">
+      //   <div className="burger">
+      //       <i className="fa-solid fa-bars"></i>
+            
+      //   </div>
+      //   <div className="mobile-logo">
+      //       <img src={logo} alt='logo'></img>
+      //   </div>
+      //   <ul className="nav-links">
+      //       <li>Home</li>
+      //       <li>Our Foods</li>
+      //       <li>Story</li>
+      //     <li className="logo"><img src={logo} alt='logo'></img></li>
+      //     <li>Contact Us</li>
+      //     <li>Login</li>
+      //     <li>Sign Up</li>
+      //   </ul>
+      // </nav>
+
           )}
-        </nav>
-      </div>
+        
+     
     </header>
   )
 }
