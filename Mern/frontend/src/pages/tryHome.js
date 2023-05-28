c685d187154f28a977b559c368954f35d2abcd5d
Lonerayd <dumalaog@yahoo.com>
2023-05-28 17:50:12 +0800

    update search with pagination

--- Mern/frontend/src/pages/tryHome.js
+++ Mern/frontend/src/pages/tryHome.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails';
@@ -11,6 +11,8 @@ const Home = () => {
   const { user } = useAuthContext();
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredWorkouts, setFilteredWorkouts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const workoutsPerPage = 2; // Number of workouts to display per page
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -31,18 +33,32 @@ const Home = () => {
 
   useEffect(() => {
     // Filter workouts based on the search query
-    
-    const filtered = workouts ? workouts.filter((workout) => {
-      const title = workout.title.toLowerCase();
-      return title.includes(searchQuery.toLowerCase());
-    }):[];
+    const filtered = workouts
+      ? workouts.filter((workout) => {
+          const title = workout.title.toLowerCase();
+          return title.includes(searchQuery.toLowerCase());
+        })
+      : [];
 
     setFilteredWorkouts(filtered);
+    setCurrentPage(1); // Reset the current page to the first page
   }, [workouts, searchQuery]);
+
+  // Pagination
+  const indexOfLastWorkout = currentPage * workoutsPerPage;
+  const indexOfFirstWorkout = indexOfLastWorkout - workoutsPerPage;
+  const currentWorkouts = filteredWorkouts.slice(indexOfFirstWorkout, indexOfLastWorkout);
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
   };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Calculate total number of pages
+  const totalPages = Math.ceil(filteredWorkouts.length / workoutsPerPage);
 
   return (
     <div className="home">
@@ -50,8 +66,20 @@ const Home = () => {
         <input type="text" value={searchQuery} onChange={handleSearch} placeholder="Search workouts" />
       </div>
       <div className="workouts">
-        {filteredWorkouts.map((workout) => (
+        {currentWorkouts.map((workout) => (
           <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
+      {/* Pagination */}
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </button>
         ))}
       </div>
       <WorkoutForm />
