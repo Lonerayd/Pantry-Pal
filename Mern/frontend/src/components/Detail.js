b95d4a083f8ebd0822d10b64ad92faabb5be216c
Miguelle-Obispo <miguelleobispo@gmail.com>
2023-05-29 11:50:22 +0800

    our story pg js

--- Mern/frontend/src/components/Detail.js
+++ Mern/frontend/src/components/Detail.js
@@ -1,8 +1,9 @@
-import { NavLink } from 'react-router-dom'
-import Detail from './css/detail.module.css'
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import { NavLink } from 'react-router-dom';
+import Detail from './css/detail.module.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
-import Dish from 'https://www.unileverfoodsolutions.com.ph/chef-inspiration/food-delivery/10-crowd-favorite-filipino-dishes/jcr:content/parsys/set1/row2/span12/columncontrol_copy_c_1458087071/columnctrl_parsys_2/textimage_copy/image.transform/jpeg-optimized/image.1637868215411.jpg' 
+import Dish from 'https://www.unileverfoodsolutions.com.ph/chef-inspiration/food-delivery/10-crowd-favorite-filipino-dishes/jcr:content/parsys/set1/row2/span12/columncontrol_copy_c_1458087071/columnctrl_parsys_2/textimage_copy/image.transform/jpeg-optimized/image.1637868215411.jpg';
+
 
 const Detailcomp = () => {
 
@@ -48,7 +49,7 @@ const Detailcomp = () => {
 
             <form className={Detail['d-flex']} role="search">
               <input className={`${Detail.form-control} ${Detail.me-2} ${Detail.search-bar}`} type="search" placeholder="Search" aria-label="Search"></input>
-              <button type="button" className={`${Detail.btn} ${Detail.btn-outline-danger} ${Detail.search-button}`}type="submit">Search</button>
+              <button type="button" className={`${Detail.btn} ${Detail.btn-outline-danger} ${Detail.search-button}`}>Search</button>
               <div className={Detail['small']}>
                 <i className={`${Detail.fa-solid} ${Detail.fa-user} ${Detail.fa-xl}`} style="color: #970c10;"></i>
               </div>
@@ -65,7 +66,7 @@ const Detailcomp = () => {
             <p>8 recipes</p> --> */}
           </div>
           <div className={Detail['filter-button']}>
-            <button type="button" className={`${Detail.btn} ${Detail.btn-outline-danger} ${Detail.search-button}`} type="submit">
+            <button type="button" className={`${Detail.btn} ${Detail.btn-outline-danger} ${Detail.search-button}`}>
                 Share
             </button>
           </div>
@@ -100,15 +101,15 @@ const Detailcomp = () => {
                         Directions
                       </p>
                       
-                      <b>1.</b> In a pot, add a bit of oil and sauté garlic, onions until translucent.<br>
-                      <b>2.</b> Add the tomatoes and sauté until dry.<br>
-                      <b>3.</b> Add the miso and slightly toast it.<br>
-                      <b>4.</b> Add the water and radish. Bring to a boil.<br>
-                      <b>5.</b> Simmer for 20 minutes until the radish is tender.<br>
-                      <b>6.</b> Add the salmon belly and Knorr Sinigang Mix.<br>
-                      <b>7.</b> Season with salt and pepper. Cook for around 8 minutes more.<br>
-                      <b>8.</b> Cook mustard leaves for 3 minutes and add the calamansi juice.<br>
-                      <b>9.</b> Serve the dish piping hot.<br>
+                      <b>1.</b> In a pot, add a bit of oil and sauté garlic, onions until translucent.<br/>
+                      <b>2.</b> Add the tomatoes and sauté until dry.<br/>
+                      <b>3.</b> Add the miso and slightly toast it.<br/>
+                      <b>4.</b> Add the water and radish. Bring to a boil.<br/>
+                      <b>5.</b> Simmer for 20 minutes until the radish is tender.<br/>
+                      <b>6.</b> Add the salmon belly and Knorr Sinigang Mix.<br/>
+                      <b>7.</b> Season with salt and pepper. Cook for around 8 minutes more.<br/>
+                      <b>8.</b> Cook mustard leaves for 3 minutes and add the calamansi juice.<br/>
+                      <b>9.</b> Serve the dish piping hot.<br/>
                       
                     </div>
                   </p>
@@ -202,6 +203,7 @@ const Detailcomp = () => {
         </div>
       </div>
     </div>    
+    
   )
 }
 
---
+++ Mern/frontend/src/components/Story.js
@@ -0,0 +1,36 @@
+import Storycss from './css/style.module.css'
+
+const Storycomp = () => {
+
+  return (
+    
+    <main className={Storycss['bgImage']}>
+        <div className={Storycss['storyHeading']}>
+            <div className={Storycss['ourStoryHeading']}>
+                <h1 className={Storycss['ourStory']}>Our Story</h1>
+            </div>
+            <div className={Storycss['storyPage']}>
+                <p>
+                    The story of Pantry Pal began with a group of friends who recognized the need for a platform where people could come together, virtually, to celebrate the culinary heritage of the Philippines. With a vision to bridge the gap between cultures and connect individuals through the love of food, they embarked on an exciting journey to create a digital haven for recipe-sharing. <br/> <br/>
+
+                    As Pantry Pal flourished, its community grew exponentially. Home cooks, professional chefs, and everyone in between united under one virtual roof to exchange culinary wisdom, share beloved family recipes, and embark on gastronomic adventures. The website became a treasure trove of culinary knowledge, where traditional Filipino dishes were meticulously documented, celebrated, and given new life.
+                    Pantry Pal's success lay in its commitment to authenticity and inclusivity. While traditional recipes like adobo, sinigang, and lechon took center stage, regional specialties and lesser-known delicacies from Luzon, Visayas, and Mindanao were equally cherished. The platform became a melting pot of flavors, where Filipino culture was beautifully represented through the diversity of its culinary traditions.
+                    To ensure a seamless user experience, Pantry Pal's website featured an intuitive interface where users could easily navigate through various categories, such as breakfast delights, savory snacks, festive feasts, and delectable desserts. Members could contribute their own recipes, accompanied by vibrant photos and detailed instructions, allowing others to recreate these culinary delights in their own kitchens.
+                    But Pantry Pal was not merely a recipe-sharing website; it fostered a sense of community and camaraderie among its users. Discussion forums, comment sections, and private messaging options encouraged interaction, enabling users to exchange tips, share personal anecdotes, and connect with fellow food enthusiasts. The website also featured a blog section, where aspiring food writers could showcase their talent and explore the stories behind the recipes. <br/><br/>
+                    
+                    As Pantry Pal continued to grow, it ventured beyond the virtual realm. The team organized exciting events such as cooking workshops, pop-up markets, and even culinary tours across the Philippines. These endeavors aimed to bring the essence of Pantry Pal to life, allowing members to forge genuine connections and experience the flavors of the archipelago firsthand. <br/><br/>
+                    
+                    With each passing day, Pantry Pal became a testament to the power of food in transcending cultural barriers and bringing people together. It celebrated the Filipino spirit, capturing the essence of fiestas, family gatherings, and the shared joy of breaking bread. Through its platform, Pantry Pal nurtured a sense of pride in Filipino cuisine, encouraging individuals worldwide to embrace and explore its unique flavors.
+                    Today, Pantry Pal stands as an emblem of Filipino culinary heritage, inspiring a new generation to savor the distinct tastes and vibrant colors of the archipelago. Its website continues to be a haven for culinary enthusiasts seeking to connect, learn, and share their love for Filipino food. As Pantry Pal expands its reach, it invites everyone to join its ever-growing family, where taste buds dance, flavors unite, and stories unfold, one recipe at a time.
+                  </p>
+            </div>
+        </div>
+        
+    </main>
+
+
+
+  )
+}
+
+export default Storycomp
