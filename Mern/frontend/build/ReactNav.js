4780f09021055fe38a3ac641607f256406c88e82
Lonerayd <dumalaog@yahoo.com>
2023-05-30 16:04:19 +0800

    build

---
+++ Mern/frontend/build/ReactNav.js
@@ -0,0 +1,39 @@
+
+
+  
+// import NavCSS from './Nav.module.css'
+
+
+
+
+            window.addEventListener("scroll", function() {
+                var navbar = document.querySelector(".navbar");
+                navbar.classList.toggle("scrolled", window.scrollY > 50);
+            
+               
+              });
+          
+            
+                
+            
+          
+            const links = document.querySelector(".nav-links");
+                    const burger = document.querySelector(".burger");
+                    const ul = document.querySelector("ul");
+                   
+                    // burger.addEventListener('click', ()=>{ul.classList.toggle('show');})
+            
+                    document.addEventListener('click', (event) => {
+                        const targetElement = event.target;
+                        if (!burger.contains(targetElement)) {
+                            links.classList.remove('show');
+                        }
+                        if (burger.contains(targetElement)){
+                            ul.classList.toggle('show')
+                        }
+                        });
+                    // story js
+          
+            
+        
+           
